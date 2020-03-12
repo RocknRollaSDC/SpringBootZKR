0384a0d89456363b3197247059e4760837870c54
84221137@qq.com <84221137@qq.com>
2020-03-13 06:40:46 +0800

    一、SpringBoot多层结构创建完成。

---
+++ cumulative_risk_coverage/src/main/java/com/abchinafile/cumulative_risk_coverage/CumulativeRiskCoverageApplication.java
@@ -0,0 +1,13 @@
+package com.abchinafile.cumulative_risk_coverage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CumulativeRiskCoverageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CumulativeRiskCoverageApplication.class, args);
+    }
+
+}
---
+++ cumulative_risk_coverage/src/test/java/com/abchinafile/cumulative_risk_coverage/CumulativeRiskCoverageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abchinafile.cumulative_risk_coverage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CumulativeRiskCoverageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
